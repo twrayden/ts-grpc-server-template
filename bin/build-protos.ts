089f87c2f3716b9b3de037ee3e5236aeac0336d6
Thomas Rayden <twrayden@gmail.com>
2022-04-16 00:47:25 +0800

    Compile every file in protos

--- bin/build-protos.ts
+++ bin/build-protos.ts
@@ -1,12 +1,7 @@
 import path from "path";
 import execa from "execa";
 import del from "del";
-
-const [inputProto] = process.argv.slice(2);
-
-if (!inputProto) {
-  throw new Error("Must provide input proto filename");
-}
+import fs from "fs";
 
 const PROJECT_DIR = path.join(__dirname, "..");
 const NODE_MODULES_DIR = path.join(PROJECT_DIR, "node_modules");
@@ -18,8 +13,6 @@ const TOOLS_DIR = path.join(NODE_MODULES_DIR, "grpc-tools/bin");
 const PROTOC_PATH = path.join(TOOLS_DIR, "protoc");
 const PLUGIN_PATH = path.join(NODE_MODULES_DIR, ".bin/protoc-gen-ts_proto");
 
-const INPUT_PROTO_PATH = path.join(PROTOS_DIR, inputProto);
-
 // https://github.com/stephenh/ts-proto#usage
 const args = [
   `--plugin=${PLUGIN_PATH}`,
@@ -27,13 +20,17 @@ const args = [
   "--ts_proto_opt=outputServices=grpc-js,env=node,useOptionals=messages,exportCommonSymbols=false,esModuleInterop=true",
   `--ts_proto_out=${MODELS_DIR}`,
   `--proto_path=${PROTOS_DIR}`,
-  INPUT_PROTO_PATH,
 ];
 
 export const run = async () => {
   await del([`${MODELS_DIR}/*`], { ignore: [`${MODELS_DIR}/tsconfig.json`] });
 
-  await execa(PROTOC_PATH, args);
+  const protos = fs.readdirSync(PROTOS_DIR);
+
+  for (const p of protos) {
+    console.log(p);
+    await execa(PROTOC_PATH, [...args, p]);
+  }
 };
 
 run();
